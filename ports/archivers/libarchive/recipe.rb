72bcbb8ce479f998b8a3b217bb3f61fa8e3ff084
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-10-30 10:49:28 -0700

    libarchive building on windows

--- ports/archivers/libarchive/recipe.rb
+++ ports/archivers/libarchive/recipe.rb
@@ -1,43 +1,59 @@
 {
-  :url => 'http://libarchive.googlecode.com/files/libarchive-2.7.1.tar.gz',
-  :md5 => 'f43382413b4457d0e192771b100a66e7',
-  :configure => lambda { |c|
-    if c[:platform] == :Darwin
-      ENV['CFLAGS'] = ENV['CFLAGS'].to_s + c[:darwinCompatCompileFlags]
-      ENV['CXXFLAGS'] = ENV['CXXFLAGS'].to_s + c[:darwinCompatCompileFlags]
-    end
+  :url => 'http://libarchive.googlecode.com/files/libarchive-2.6.2.tar.gz',
+  :md5 => 'e31fcacd3f2b996988c0852a5edfc680',
+  :configure => {
+    [:Linux, :MacOSX] => lambda { |c|
+      if c[:platform] == :Darwin
+        ENV['CFLAGS'] = ENV['CFLAGS'].to_s + c[:darwinCompatCompileFlags]
+        ENV['CXXFLAGS'] = ENV['CXXFLAGS'].to_s + c[:darwinCompatCompileFlags]
+      end
 
-    if c[:build_type] == :debug
-      ENV['CFLAGS'] = ENV['CFLAGS'].to_s + " -g -O0"
-    end
+      if c[:build_type] == :debug
+        ENV['CFLAGS'] = ENV['CFLAGS'].to_s + " -g -O0"
+      end
 
-    configCmd = File.join(c[:src_dir], "configure")    
-    configCmd += " --without-lzmadec --without-bz2lib"
-    configCmd += " --without-zlib --disable-shared --disable-bsdcpio"
-    configCmd += " --disable-bsdtar --enable-static --prefix=#{c[:output_dir]}"
-    system(configCmd)
-    puts "config cmd: #{configCmd}"
+      configCmd = File.join(c[:src_dir], "configure")    
+      configCmd += " --without-lzmadec --without-bz2lib"
+      configCmd += " --without-zlib --disable-shared --disable-bsdcpio"
+      configCmd += " --disable-bsdtar --enable-static --prefix=#{c[:output_dir]}"
+      system(configCmd)
+      puts "config cmd: #{configCmd}"
+    }
   },
   :build => {
     :Windows => lambda { |c|
       buildStr = c[:build_type].to_s.capitalize
-      system("devenv YetAnotherJSONParser.sln /Build #{buildStr}")
+      pathToSoln = File.join(c[:src_dir], "windows", "vc90", "libarchive.sln")
+      pathToSoln.gsub!(/\//, "\\")
+      buildCmd = "devenv \"#{pathToSoln}\" /Build #{buildStr}"
+      puts buildCmd
+      system(buildCmd)
     },
     :MacOSX => "make", 
     :Linux => "make"
   },
-  :install => lambda { |c|
-    if c[:platform] == :Windows
-      system("make install")
-      # now let's move output to the appropriate place
-      # i.e. move from lib/libfoo.a to lib/debug/foo.a
-      libdir = File.join(c[:output_dir], "lib", c[:build_type].to_s)
-      FileUtils.mkdir_p(libdir)
-      Dir.glob(File.join(c[:output_dir], "lib", "*")).each { |f|
-        FileUtils.mv(f, libdir) if !File.directory? f
-      }
-    else
-      system("make install")
-    end
+  :install => {
+    :Windows => lambda { |c|
+      # set up directories
+      hdr_dir = File.join(c[:output_dir], "include", "libarchive") 
+      lib_dir = File.join(c[:output_dir], "lib", c[:build_type].to_s)
+      FileUtils.mkdir_p(hdr_dir)
+      FileUtils.mkdir_p(lib_dir)
+
+      puts "Installing libs..."
+      debug_str = (c[:build_type] == :debug) ? "-d" : ""
+      src = File.join(c[:src_dir], "lib", "libarchive-vc90-mt#{debug_str}.lib")
+      dst = File.join(lib_dir, "archive_s.lib")
+      puts "copying from #{src} to #{dst}"
+      FileUtils.cp(src, dst, :verbose => true)
+
+      puts "Installing headers..."
+      [ "archive.h", "archive_entry.h" ].each do |h|
+        FileUtils.cp(File.join(c[:src_dir], "libarchive", h), hdr_dir,
+                     :verbose => true)
+      end
+    },
+    [:Linux, :MacOSX] => "make install"
   }
 }
+
