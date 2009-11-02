d4e4c5d2f39f509f4b211cd38677c507f4bcba06
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-11-02 13:48:24 -0700

    refine libjpeg build on osx

--- ports/graphics/jpeg/recipe.rb
+++ ports/graphics/jpeg/recipe.rb
@@ -7,15 +7,27 @@
       ENV['CFLAGS'] += ' -g -O0 ' if c[:build_type] == :debug
       ENV['LDFLAGS'] = "#{c[:os_link_flags]} #{ENV['LDFLAGS']}"
       cfgScript = File.join(c[:src_dir], "configure")
-      cfgOpts = "--enable-static --disable-shared --prefix=\"#{c[:output_dir]}\""
+      cfgOpts = "--prefix=\"#{c[:output_dir]}\""
       puts "#{cfgScript} #{cfgOpts}"
       system("#{cfgScript} #{cfgOpts}")
     }    
   },
   :build => {
-    [ :Linux, :MacOSX ] => "make"
+    [ :Linux, :MacOSX ] => "make libjpeg.a"
   },
   :install => {
-    [ :Linux, :MacOSX ] => "make install"
+    [ :Linux, :MacOSX ] => lambda { |c|
+      system("make install-lib")
+
+      # move headers into place
+      Dir.glob(File.join(c[:output_dir], "include", "j*h")).each { |h|
+        FileUtils.mv(h, c[:output_inc_dir])
+      }
+
+      # move library into place
+      FileUtils.mv(File.join(c[:output_dir], "lib", "libjpeg.a"),
+                   File.join(c[:output_lib_dir], "libjpeg_s.a"),
+                   :verbose => true)
+    }
   }
 }
