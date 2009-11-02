f36ee59d218b0b88ea57c0f203eb82ad56e0a146
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-11-02 13:20:42 -0800

    zlib builds on win32

--- ports/archivers/zlib/recipe.rb
+++ ports/archivers/zlib/recipe.rb
@@ -8,7 +8,8 @@
         ENV['CFLAGS'] = "-g -O0 #{ENV['CFLAGS']}" if (c[:build_type] == :debug)
         system("./configure")
       }
-    }
+    },
+    :Windows => "echo no configuration required"
   },
   :build => {
     [ :Linux, :MacOSX ] => lambda { |c|
@@ -16,6 +17,13 @@
       Dir.chdir(c[:src_dir]) {
         system("make")
       }
+    },
+    :Windows => lambda { |c|
+      pathToSLN = File.join(c[:src_dir], "projects", "visualc6", "zlib.sln")
+      bt = c[:build_type].to_s.capitalize
+      devenvCmd = "devenv #{pathToSLN} /build \"LIB #{bt}\""
+      puts "issuing: #{devenvCmd}"
+      system(devenvCmd)
     }
   },
   :install => {
@@ -24,12 +32,23 @@
       FileUtils.cp(File.join(c[:src_dir], "libz.a"),
                    File.join(c[:output_lib_dir], "libzlib_s.a"),
                    :verbose => true)
-
+    },
+    :Windows => lambda { |c|
+      bt = c[:build_type].to_s.capitalize      
+      suffix = (bt == "Debug") ? "d" : ""
+      FileUtils.install(File.join(c[:src_dir], "projects", "visualc6",
+                                  "Win32_LIB_#{bt}", "zlib#{suffix}.lib"),
+                        File.join(c[:output_lib_dir], "zlib_s.lib"),
+                        :verbose => true)
+    }
+  },
+  :post_install => lambda { |c|
+    if c[:build_type] == :release
       puts "installing headers..."
       ["zconf.h", "zlib.h"].each { |h|
         FileUtils.cp(File.join(c[:src_dir], h),
                      c[:output_inc_dir], :verbose => true)
       }
-    }
+    end
   }
-}  
+}
