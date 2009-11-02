03f1d0d421c928ed8367242f08e9d2d03917ed29
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-11-02 13:33:57 -0800

    jpeg building on win32

--- ports/graphics/jpeg/recipe.rb
+++ ports/graphics/jpeg/recipe.rb
@@ -10,10 +10,20 @@
       cfgOpts = "--prefix=\"#{c[:output_dir]}\""
       puts "#{cfgScript} #{cfgOpts}"
       system("#{cfgScript} #{cfgOpts}")
-    }    
+    },
+    :Windows => "echo no configuration required on windows"
   },
   :build => {
-    [ :Linux, :MacOSX ] => "make libjpeg.a"
+    [ :Linux, :MacOSX ] => "make libjpeg.a",
+    :Windows => lambda { |c|
+      Dir.chdir(c[:src_dir]) { 
+        makeFilePath = File.join(c[:src_dir], "makefile.vc")
+        makeCmd = "nmake /f \"#{makeFilePath}\""
+        makeCmd += " nodebug=1" if c[:build_type] == :release
+        FileUtils.cp("jconfig.vc", "jconfig.h", :verbose => true)
+        system(makeCmd)
+      }
+    }
   },
   :install => {
     [ :Linux, :MacOSX ] => lambda { |c|
@@ -28,6 +38,21 @@
       FileUtils.mv(File.join(c[:output_dir], "lib", "libjpeg.a"),
                    File.join(c[:output_lib_dir], "libjpeg_s.a"),
                    :verbose => true)
+    },
+    :Windows => lambda { |c| 
+      puts "install lib..."
+      FileUtils.install(File.join(c[:src_dir], "libjpeg.lib"),
+                        File.join(c[:output_lib_dir], "jpeg.lib"),
+                        :verbose => true)
+
+      if c[:build_type] == :release
+        puts "installing headers..."
+        ["jconfig.h", "jerror.h", "jmorecfg.h", "jpeglib.h"].each do |f|
+          FileUtils.install(File.join(c[:src_dir], f),
+                            c[:output_inc_dir],
+                            :preserve => true, :verbose => true)
+        end
+      end
     }
   }
 }
