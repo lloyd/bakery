fdfcd75d028f507f41845be5c7f95786f6358741
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-11-03 12:42:10 -0800

    (GraphicsMagick) patch in visual studio build files generated by VisualMagick then hand massaged (ouch!)

--- ports/graphics/GraphicsMagick/recipe.rb
+++ ports/graphics/GraphicsMagick/recipe.rb
@@ -1,6 +1,36 @@
 {
   :url => 'http://softlayer.dl.sourceforge.net/project/graphicsmagick/graphicsmagick/1.3.7/GraphicsMagick-1.3.7.tar.bz2',
   :md5 => '42bfd382ddcda399880721170bcbf61b',
+  :post_patch => {
+    :Windows => lambda { |c| 
+      puts "Running post-patch"
+      # Now ladies and gentlemen, we'll go through all vcproj files and
+      # replace a couple paths with their actual path
+      inc_dir = File.join(c[:output_dir], "include")
+      actualPaths = {
+        /ZLIB_INCLUDE_PATH/ => File.join(inc_dir, "zlib"),
+        /JPEG_INCLUDE_PATH/ => File.join(inc_dir, "jpeg"),
+        /LIBPNG_INCLUDE_PATH/ => File.join(inc_dir, "libpng")
+      }
+
+      # massage pathery
+      actualPaths.each{ |k,v|
+        actualPaths[k] = File.expand_path(v).gsub(/\//,"\\")
+      }
+        
+      Dir.glob(File.join(c[:src_dir], "VisualMagick", "**", "*.vcproj")).each { |p|
+        puts "subbing '#{p}'"
+        # read the whole thing
+        contents = File.read(p)
+
+        # sub in the proper paths
+        actualPaths.each { |k,v| contents.gsub!(k, v) }
+        
+        # write the whole thing
+        File.open(p, "w") { |f| f.write contents }
+      }
+    }
+  },
   :configure => {
     [ :Linux, :MacOSX ] => lambda { |c|
       jpg_hdrs = File.join(c[:output_dir], "include", "jpeg")
