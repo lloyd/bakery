bba0fd98b54969a24b49fa64359979670c27eb2b
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-11-19 21:49:21 -0800

    (jsmin) trivial win32 port

--- ports/devel/jsmin/recipe.rb
+++ ports/devel/jsmin/recipe.rb
@@ -4,14 +4,16 @@
     puts c[:source_dir]
     jsminSrc = File.join(c[:recipe_dir], "jsmin.c")
     if c[:platform] == :Windows
-      raise "not yet implemented!  gotta compile! XXX"
+      cmd = "cl #{jsminSrc}"
+      system(cmd)
     else
       cmd = "gcc -Wall -g -o jsmin #{jsminSrc}"
       system(cmd)
     end
   },
   :install => lambda { |c|
-    FileUtils.cp("jsmin", c[:output_bin_dir],
+    ext = (c[:platform] == :Windows) ? ".exe" : ""
+    FileUtils.cp("jsmin#{ext}", c[:output_bin_dir],
                  { :verbose => true, :preserve => true })
   }
 }
