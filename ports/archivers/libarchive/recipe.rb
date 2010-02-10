1dbc6e8ecf15f38a0e755b5418f50a8d85a2f405
Gordon Durand <gordon_durand@yahoo.com>
2010-02-10 14:57:37 -0800

    Move to libarchive 2.8.0

--- ports/archivers/libarchive/recipe.rb
+++ ports/archivers/libarchive/recipe.rb
@@ -1,69 +1,93 @@
+# use nmake on windows, faster than vs.  however, vs can 
+# be handy for debugging
+useNMake = true
+
+def setupEnv(c) 
+  if c[:platform] != :Windows
+    ENV['CFLAGS'] = ENV['CFLAGS'].to_s + c[:os_compile_flags]
+    if c[:build_type] == :debug
+      ENV['CFLAGS'] = ENV['CFLAGS'].to_s + " -g -O0"
+    end
+    ENV['LDFLAGS'] = ENV['LDFLAGS'].to_s + c[:os_link_flags]
+  end
+end
+
 {
-  :url => 'http://libarchive.googlecode.com/files/libarchive-2.6.2.tar.gz',
-  :md5 => 'e31fcacd3f2b996988c0852a5edfc680',
-  :configure => {
-    [:Linux, :MacOSX] => lambda { |c|
-      if c[:platform] == :MacOSX
-        ENV['CFLAGS'] = ENV['CFLAGS'].to_s + c[:os_compile_flags]
-        ENV['CXXFLAGS'] = ENV['CXXFLAGS'].to_s + c[:os_compile_flags]
-        ENV['LDFLAGS'] = ENV['LDFLAGS'].to_s + c[:os_link_flags]
-      end
+  :url => 'http://libarchive.googlecode.com/files/libarchive-2.8.0.tar.gz',
+  :md5 => '400fd9ba51fffe6c65c75387fffba9d9',
 
-      if c[:build_type] == :debug
-        ENV['CFLAGS'] = ENV['CFLAGS'].to_s + " -g -O0"
-      end
+  :configure => lambda { |c|
+    realSrcDir = c[:src_dir]
+    installDir = File.join(c[:build_dir], "install")
+    zlibIncDir = File.join(File.dirname(c[:output_inc_dir]), "zlib")
+    bzip2IncDir = File.join(File.dirname(c[:output_inc_dir]), "bzip2")
+    if c[:platform] == :Windows
+      realSrcDir = File.expand_path(realSrcDir).gsub(/\//, "\\")
+      installDir = File.expand_path(installDir).gsub(/\//, "\\")
+      zlibIncDir = File.expand_path(zlibIncDir).gsub(/\//, "\\")
+      bzip2IncDir = File.expand_path(bzip2IncDir).gsub(/\//, "\\")
+    end
 
-      stageDir = File.join(File.dirname(c[:build_dir]), "stage")
-      configCmd = File.join(c[:src_dir], "configure")    
-      configCmd += " --without-lzmadec --without-bz2lib"
-      configCmd += " --without-zlib --disable-shared --disable-bsdcpio"
-      configCmd += " --disable-bsdtar --enable-static --prefix=#{stageDir}"
-      system(configCmd)
-      puts "config cmd: #{configCmd}"
-    }
+    # setup compile/link flags
+    setupEnv(c)
+
+    # add env vars to allow cmake to find zlib/bzip2
+    ENV['ZLIB_INC_DIR'] = zlibIncDir
+    ENV['EXT_LIB_DIR'] = c[:output_lib_dir]
+    ENV['BZIP2_INC_DIR'] = bzip2IncDir
+
+    cmakeGen = nil
+    if useNMake
+      cmakeGen = "-G \"NMake Makefiles\"" if c[:platform] == :Windows
+    else
+      cmakeGen = "-G \"#{c[:cmake_generator]}\"" if c[:cmake_generator]
+    end
+    buildType = "-DCMAKE_BUILD_TYPE:STRING=#{c[:build_type].to_s.capitalize}"
+    cmakeVars = "-DCMAKE_INSTALL_PREFIX:PATH=#{installDir} -DENABLE_ACL:BOOL=OFF -DENABLE_XATTR:BOOL=OFF"
+    
+    # XXX, test_write_compress_program fails on doze, causes devenv to fail
+    if c[:platform] == :Windows
+      cmakeVars += " -DENABLE_TEST:BOOL=OFF"
+    end
+
+    cmLine = "cmake #{cmakeGen} #{buildType} #{c[:cmake_args]} #{cmakeVars} " + " \"#{realSrcDir}\"" 
+    puts "config cmd: #{cmLine}"
+    system(cmLine)
   },
-  :build => {
-    :Windows => lambda { |c|
-      buildStr = c[:build_type].to_s.capitalize
-      pathToSoln = File.join(c[:src_dir], "windows", "vc90", "libarchive.sln")
-      pathToSoln.gsub!(/\//, "\\")
-      buildCmd = "devenv \"#{pathToSoln}\" /Build #{buildStr}"
-      puts buildCmd
-      system(buildCmd)
-    },
-    :MacOSX => "make",
-    :Linux => "make"
+
+  :build => lambda { |c|
+    # setup compile/link flags
+    setupEnv(c)
+    
+    if c[:platform] == :Windows && !useNMake
+      buildType = c[:build_type].to_s.capitalize
+      system("devenv libarchive.sln /build #{buildType}")
+      system("devenv libarchive.sln /build #{buildType} /project INSTALL")
+    else
+      makeCmd = c[:platform] == :Windows ? "nmake" : "make"
+      system("#{makeCmd} -e")
+      system("#{makeCmd} -e install")
+    end
   },
-  :install => {
-    :Windows => lambda { |c|
-      puts "Installing libs..."
-      debug_str = (c[:build_type] == :debug) ? "-d" : ""
-      src = File.join(c[:src_dir], "lib", "libarchive-vc90-mt#{debug_str}.lib")
+
+  :install => lambda { |c|
+    puts "Installing libs..."
+    src = File.join(c[:build_dir], "install", "lib")
+    if c[:platform] == :Windows
+      src = File.join(src, "archive_static.lib")
       dst = File.join(c[:output_lib_dir], "archive_s.lib")
-      puts "copying from #{src} to #{dst}"
-      FileUtils.cp(src, dst, :verbose => true)
+    else 
+      src = File.join(src, "libarchive.a")
+      dst = File.join(c[:output_lib_dir], "libarchive_s.a")
+    end
+    puts "copying from #{src} to #{dst}"
+    FileUtils.cp(src, dst, :verbose => true)
 
-      puts "Installing headers..."
-      [ "archive.h", "archive_entry.h" ].each do |h|
-        FileUtils.cp(File.join(c[:src_dir], "libarchive", h),
+    puts "Installing headers..."
+    [ "archive.h", "archive_entry.h" ].each do |h|
+        FileUtils.cp(File.join(c[:build_dir], "install", "include", h),
                      c[:output_inc_dir], :verbose => true)
       end
-    },
-    [:Linux, :MacOSX] => lambda { |c|
-      stageDir = File.join(File.dirname(c[:build_dir]), "stage")
-      system("make install")
-
-      puts "Installing headers..."
-      [ "archive.h", "archive_entry.h" ].each do |h|
-        FileUtils.cp(File.join(stageDir, "include", h),
-                     c[:output_inc_dir], :verbose => true)
-      end
-
-      puts "Installing static library..."
-      FileUtils.cp(File.join(stageDir, "lib", "libarchive.a"),
-                   File.join(c[:output_lib_dir], "libarchive_s.a"),
-                   :verbose => true)
-    }      
   }
 }
 
