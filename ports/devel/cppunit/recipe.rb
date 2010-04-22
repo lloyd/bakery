c4fc92475032b28cb38782677922fbb9f00a47c5
Gordon Durand <gordon_durand@yahoo.com>
2010-04-21 18:06:50 -0700

    get cppunit building on osx

--- ports/devel/cppunit/recipe.rb
+++ ports/devel/cppunit/recipe.rb
@@ -4,54 +4,59 @@
   :md5 => "bd30e9cf5523cdfc019b94f5e1d7fd19",
   :configure => {
     [ :Linux, :MacOSX ] => lambda { |c|
-      puts "Building #{c[:build_type].to_s} bits..."
+      puts "Configuring #{c[:build_type].to_s} bits..."
       ldflags = ""
-      cflags = c[:build_type] == :debug ? "-g -O0" : ""
+      cxxflags = c[:build_type] == :debug ? "-g -O0" : ""
       if c[:platform] == :MacOSX
-        cflags += c[:os_compile_flags]
+        cxxflags += c[:os_compile_flags]
       end
       cfgScript = File.join(c[:src_dir], "configure")
       configCmd = "#{cfgScript} --enable-static --disable-shared"
-      configCmd += " --prefix=#{c[:output_dir]} --disable-doxygen"
-      configCmd += " CXXFLAGS=\"#{cflags}\""
-      if $platform == :MacOSX
+      configCmd += " --prefix=#{c[:build_dir]} --disable-doxygen"
+      configCmd += " CXXFLAGS=\"#{cxxflags}\""
+      if c[:platform] == :MacOSX
         configCmd += " LDFLAGS=\"#{c[:os_link_flags]}\""
       end
-      system(configCmd)
+      # the '|| echo' prevents termination since configCmd fails.  nice
+      system("#{configCmd} || echo")
     }
   },
+
   :build => {
-    [ :Linux, :MacOSX ] => "make",
+    [ :Linux, :MacOSX ] => lambda { |c|
+      system("make")
+      system("make install")
+    },
     :Windows => lambda { |c|
-      Dir.chdir(File.join(c[:src_dir], "src")) { 
-        buildType = c[:build_type].to_s
-        system("devenv CppUnitLibraries.sln /build \"#{buildType} static\"")
-      }
+      buildType = c[:build_type].to_s
+      system("devenv CppUnitLibraries.sln /build \"#{buildType} static\"")
     }
   },
+
   :install => {
     [ :Linux, :MacOSX ] => lambda { |c|
-      system("make install")
-
-      # now move built libraries down into a build type specific dir
-      Dir.glob(File.join(c[:output_dir], "lib", "*cppunit*")).each { |f|
-        FileUtils.mv(f, c[:output_lib_dir], :verbose => true)
+      Dir.glob(File.join(c[:build_dir], "lib", "*cppunit*")).each { |f|
+        FileUtils.cp(f, c[:output_lib_dir], :verbose => true)
       }
     },
+
     :Windows => lambda { |c|
       # install static lib
       puts "installing #{c[:build_type].to_s} static library..."
       libTrailer = (c[:build_type] == :debug) ? "d" : ""
-
       FileUtils.cp(File.join(c[:build_dir], "lib", "cppunit#{libTrailer}.lib"),
                    File.join(c[:output_lib_dir], "cppunit.lib"),
                    :verbose => true)
+    }
+  },
 
-      # install headers
-      puts "installing headers..."
-      Dir.glob(File.join(File.join(c[:src_dir], "include", "cppunit", "*"))).each { |h| 
-        FileUtils.cp_r(h, c[:output_inc_dir], :verbose => true)
-      }
+  :post_install_common => lambda { |c|
+    # install headers
+    puts "installing headers..."
+    Dir.glob(File.join(File.join(c[:src_dir], "include",
+                                 "cppunit", "*"))).each { |h| 
+      FileUtils.cp_r(h, c[:output_inc_dir], :verbose => true)
     }
   }
 }
+
