8b6522c28a79ba44b0dd50faa12e83fa24b8128e
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-06-16 14:59:37 -0600

    (service_testing) move to latest and install json parsing ruby lib needed for testing harness to function properly on ruby 1.8

--- ports/browserplus/service_testing/recipe.rb
+++ ports/browserplus/service_testing/recipe.rb
@@ -1,11 +1,14 @@
 {
   :deps => [ "service_runner" ], 
-  :url => 'github://browserplus/service-testing/d4f5da714f4b0618925624dd541492216b9298ef',
+  :url => 'github://browserplus/service-testing/091664cbb6e94950e0b18a88e6074d11c1697619',
   :install => lambda { |c|
     tgtDir = File.join(c[:output_dir], "share", "service_testing")
     FileUtils.mkdir_p(tgtDir)
     FileUtils.cp(File.join(c[:src_dir], "ruby", "bp_service_runner.rb"),
                  tgtDir,
                  { :verbose => true, :preserve => true })
+    FileUtils.cp_r(File.join(c[:src_dir], "ruby", "json"),
+                   tgtDir,
+                   { :verbose => true, :preserve => true })
   }
 }
