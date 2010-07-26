95dbe59ac77a7a9892fd66db2c1c396a53f7e3ab
Gary MacDonald <garymd@yahoo-inc.com>
2010-07-26 15:23:01 -0700

    Bump service-testing recipe to latest commit (includes support for cppunit xml)

--- ports/browserplus/service_testing/recipe.rb
+++ ports/browserplus/service_testing/recipe.rb
@@ -1,10 +1,13 @@
 {
   :deps => [ "service_runner" ], 
-  :url => 'github://browserplus/service-testing/c9e9223e308614893277963159ab0335327b3139',
+  :url => 'github://browserplus/service-testing/154e5448716f546cda58a3577e0bedcf660b20b2',
   :install => lambda { |c|
     tgtDir = File.join(c[:output_dir], "share", "service_testing")
     FileUtils.mkdir_p(tgtDir)
     FileUtils.cp(File.join(c[:src_dir], "ruby", "bp_service_runner.rb"),
+                 tgtDir,
+                 { :verbose => true, :preserve => true })
+    FileUtils.cp(File.join(c[:src_dir], "ruby", "cppunit_runner.rb"),
                  tgtDir,
                  { :verbose => true, :preserve => true })
     FileUtils.cp_r(File.join(c[:src_dir], "ruby", "json"),
