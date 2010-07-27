7f3ce75f46e8b8a6429aff5b5fa1876334847982
Gary MacDonald <garymd@yahoo-inc.com>
2010-07-27 10:41:42 -0700

    Sample XSD file had one incorrect node name

--- ports/browserplus/service_testing/recipe.rb
+++ ports/browserplus/service_testing/recipe.rb
@@ -1,6 +1,6 @@
 {
   :deps => [ "service_runner" ], 
-  :url => 'github://browserplus/service-testing/dcdf3bd5c3195d0e1615c6faf2d8d17ca2d6c94c',
+  :url => 'github://browserplus/service-testing/10fa1fc9c1d40151dfd0853676e890d6425a7de3',
   :install => lambda { |c|
     tgtDir = File.join(c[:output_dir], "share", "service_testing")
     FileUtils.mkdir_p(tgtDir)
